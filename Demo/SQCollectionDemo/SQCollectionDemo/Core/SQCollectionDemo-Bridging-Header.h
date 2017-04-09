42f47363c89f444aa61c1476d84a1b4e3944d61b
qbshen <qbshen_whaley>
2017-04-09 19:35:56 +0800

    update

---
+++ Demo/SQCollectionDemo/SQCollectionDemo/Core/SQCollectionDemo-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  SQCollectionDemo-Bridging-Header.h
+//  SQCollectionDemo
+//
+//  Created by qbshen on 2017/4/9.
+//  Copyright © 2017年 qbshen. All rights reserved.
+//
+
+#ifndef SQCollectionDemo_Bridging_Header_h
+#define SQCollectionDemo_Bridging_Header_h
+
+
+#endif /* SQCollectionDemo_Bridging_Header_h */
